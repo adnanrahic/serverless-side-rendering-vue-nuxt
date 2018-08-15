c97ff2187377c5a2e33dc5475d010113231d439b
adnanrahic <ado.raha198@gmail.com>
2018-08-15 14:08:02 +0200

    (feature) added mime types to file, added loading mime types in serverless-http

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express()
 const { Nuxt } = require('nuxt')
 const path = require('path')
 const sls = require('serverless-http')
+const binaryMimeTypes = require('./binaryMimeTypes')
 
 app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')))
 const config = require('./nuxt.config.js')
 const nuxt = new Nuxt(config)
 app.use(nuxt.render)
 
-module.exports.run = sls(app)
+module.exports.run = sls(app, {
+  binary: binaryMimeTypes
+})
